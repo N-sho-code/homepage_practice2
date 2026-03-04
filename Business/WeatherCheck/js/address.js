e5c2cbca256353f73a4743940cb71f3fed9afcd3
N-sho-code <shou1408829@yahoo.co.jp>
2026-03-04 23:23:30 +0900

    住所設定追加

--- Business/WeatherCheck/js/address.js
+++ Business/WeatherCheck/js/address.js
@@ -1,19 +1,30 @@
-// {
-//     "13": [
-//       { "cityCode": "13101", "cityName": "千代田区" },
-//       { "cityCode": "13102", "cityName": "中央区" }
-//     ],
-//     "14": [
-//       { "cityCode": "14100", "cityName": "横浜市" },
-//       { "cityCode": "14130", "cityName": "川崎市" }
-//     ]
-//   }
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
+    // const prefSelect = document.getElementById("prefecture");
     const prefSelect = document.getElementById("prefecture");
     const citySelect = document.getElementById("city");
     const errorDiv = document.getElementById("error");
 
-    prefSelect.addEventListener("change",async function(){
-        const
+    prefSelect.addEventListener("change", async function () {
+        const prefCode = this.value;
+        //初期化
+        citySelect.innerHTML = '<option value="">市区町村を選択</option>';
+        errorDiv.textContent = "";
+        citySelect.disabled = true;
+        if (!prefCode || prefCode == "0") return;
+        try {
+            const cities = await fetchCities(prefCode);
+
+            //cities が配列であることを前提に追加
+            cities.forEach(city => {
+                const option = document.createElement("option");
+                option.value = city.cityCode;
+                option.textContent = city.cityName;
+                citySelect.appendChild(option);
+            });
+            citySelect.disabled = false;
+        } catch (error) {
+            console.error(error);
+            errorDiv.textContent = "市区町村の取得に失敗しました";
+        }
     })
 });
--- Business/WeatherCheck/js/api.js
+++ Business/WeatherCheck/js/api.js
@@ -1,12 +1,13 @@
 async function  fetchCities(prefCode){
     try{
+        //address.json が project ルートにあるなら ’./address.json’ を参照
         const response = await fetch('data/address.json');
+
         if(!response.ok){
             throw new Error("データ取得失敗");
         }
         const data =await response.json();
         return data[prefCode]||[];
-
     }catch(error){
         console.error(error);
         throw error;
