3635392d26447ff8c70c741a9fd3a12bac6cd757
N-sho-code <shou1408829@yahoo.co.jp>
2026-06-25 00:35:13 +0900

    マップ表示

--- game/Pacman/js/script.js
+++ game/Pacman/js/script.js
@@ -1,7 +1,7 @@
 const canvas =document.getElementById("canvas");
 const canvasContext = canvas.getContext("2d");
 
-const pacmanFramews = document.getElementById("animation");
+const pacmanFramews = document.getElementById("animations");
 const ghostFramews = document.getElementById("ghosts");
 
 let createRect = (x,y,width,height,color) =>{
@@ -10,7 +10,7 @@ let createRect = (x,y,width,height,color) =>{
 };// マップ設定
 let fps =30;
 let oneBlockSize =20;
-let 
+let wallColor ="#342DCA";
 let map = [
     [1,1,1,1,1, ,1,1,1,1,1 ,1,1,1,1,1 ,1,1,1,1,1, 1],
     [1,2,2,2,2, ,2,2,2,2,2 ,1,2,2,2,2 ,2,2,2,2,2, 1],
@@ -38,25 +38,26 @@ let map = [
 ];
 
 let gameLoop=()=>{
-    update()
-    draw()
+    update();
+    draw();
 };
 
 let update = ()=>{
     // todo
 };
 let draw = ()=>{
+    createRect(0,0,canvas.width,canvas.height,"black");
     // todo
-    
+    drawWalls();
 };
 
 let gameInterval = setInterval(gameLoop,1000/fps);
 
 let drawWalls = ()=>{
     for(let i=0;i<map.length;i++){
-        for(let j=0;i<map[0].length;j++){
+        for(let j=0;j<map[0].length;j++){
             if(map[i][j]==1){//then it is a wall
-                createRect(j*oneBlockSize,i*oneBlockSize,oneBlockSize,oneBlockSize,"#342DA")
+                createRect(j*oneBlockSize,i*oneBlockSize,oneBlockSize,oneBlockSize,wallColor);
             }
         }
 
