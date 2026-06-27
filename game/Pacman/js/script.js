79096aa4c39a6d94c03d6ea53c53c046642171f2
N-sho-code <shou1408829@yahoo.co.jp>
2026-06-27 17:51:51 +0900

    マップ表示の壁表示の変更

--- game/Pacman/js/script.js
+++ game/Pacman/js/script.js
@@ -7,7 +7,14 @@ const ghostFramews = document.getElementById("ghosts");
 let createRect = (x,y,width,height,color) =>{
     canvasContext.fillStyle = color;
     canvasContext.fillRect(x,y,width,height);
-};// マップ設定
+};
+let fps =30;
+let oneBlockSize =20;
+let wallColor ="#342DCA";
+let wallSpaceWidth = oneBlockSize/1.2;
+let wallOffset = (oneBlockSize - wallSpaceWidth)/2;
+let wallInnerColor = "black";
+// マップ設定
 let map = [
     [1,1,1,1,1 ,1,1,1,1,1 ,1,1,1,1,1 ,1,1,1,1,1, 1],
     [1,2,2,2,2 ,2,2,2,2,2 ,1,2,2,2,2 ,2,2,2,2,2, 1],
@@ -19,7 +26,7 @@ let map = [
     [1,1,1,1,1 ,2,1,1,1,2 ,1,2,1,1,1 ,2,1,1,1,1, 1],
     [0,0,0,0,1 ,2,1,2,2,2 ,2,2,2,2,1 ,2,1,0,0,0, 0],
     [1,1,1,1,1 ,2,1,2,1,1 ,2,1,1,2,1 ,2,1,1,1,1, 1],
-    [2,2,2,2,2 ,2,2,2,1,2 ,2,2,1,2,2 ,2,2,2,2,2, 1],
+    [2,2,2,2,2 ,2,2,2,1,2 ,2,2,1,2,2 ,2,2,2,2,2, 2],
     [1,1,1,1,1 ,2,1,2,1,2 ,2,2,1,2,1 ,2,1,1,1,1, 1],
     [0,0,0,0,1 ,2,1,2,1,1 ,1,1,1,2,1 ,2,1,0,0,0, 0],
     [0,0,0,0,1 ,2,1,2,2,2 ,2,2,2,2,1 ,2,1,0,0,0, 0],
@@ -33,10 +40,6 @@ let map = [
     [1,2,2,2,2 ,2,2,2,2,2 ,2,2,2,2,2 ,2,2,2,2,2, 1],
     [1,1,1,1,1 ,1,1,1,1,1 ,1,1,1,1,1 ,1,1,1,1,1, 1],
 ];
-let fps =30;
-let oneBlockSize =20;
-let wallColor ="#342DCA";
-
 let gameLoop=()=>{
     update();
     draw();
@@ -57,7 +60,25 @@ let drawWalls = ()=>{
     for(let i=0;i<map.length;i++){
         for(let j=0;j<map[0].length;j++){
             if(map[i][j]==1){//then it is a wall
-                createRect(j*oneBlockSize,i*oneBlockSize,oneBlockSize,oneBlockSize,wallColor);
+                createRect(j*oneBlockSize,
+                    i*oneBlockSize,
+                    oneBlockSize,
+                    oneBlockSize,
+                    wallColor);
+                if(j>0&&map[i][j-1]==1){
+                    createRect(j*oneBlockSize,
+                        i*oneBlockSize+wallOffset,
+                        wallSpaceWidth+wallOffset,
+                        wallSpaceWidth,
+                        wallInnerColor);
+                }
+                if(j<map[0].length-1&&map[i][j+1]==1){
+                    createRect(j*oneBlockSize+wallOffset,
+                        i*oneBlockSize+wallOffset,
+                        wallSpaceWidth+wallOffset,
+                        wallSpaceWidth,
+                        wallInnerColor);
+                }
             }
         }
 
